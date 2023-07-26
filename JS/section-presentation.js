941af750f6ff73bbb635af5bf80c63120b663762
Maxvaucelle <125647489+Maxvaucelle@users.noreply.github.com>
2023-07-26 19:09:49 +0200

    modif front & new files form

--- JS/section-presentation.js
+++ JS/section-presentation.js
@@ -25,35 +25,5 @@ function afficherPresentation2() {
     }
 }
 
-window.addEventListener('scroll', function() {
-    var heading = document.getElementById('heading');
-    var titleHeading = this.document.getElementById('titleHeading');
-    var hr = document.querySelector('hr');
-    var sectionPresent = document.getElementById('hiddenPresent');
-
-    var headingPosition = heading.getBoundingClientRect().top;
-    var hrPosition = hr.getBoundingClientRect().top;
-
-    if (headingPosition <= 0 && hrPosition >= headingPosition) {
-        // Masquer la div section-present
-        sectionPresent.style.visibility = 'hidden';
-        // Insérer la balise hr après le h2
-        titleHeading.insertAdjacentElement('afterend', hr);
-        // Déplacer la balise hr en dessous du h2 avec une transition de 1.5s
-        hr.style.marginTop = '20px';    
-    } else {
-        // Afficher la div section-present
-        sectionPresent.style.visibility = 'visible';
-         // Remettre la balise hr à sa position initiale (à la fin de la section)
-        hr.style.marginTop = '0';
-        // Remettre la balise hr à sa position initiale (à la fin de la section)
-        sectionPresent.insertAdjacentElement('afterend', hr);
-    }
-});
-
-
-
-
-
 // Ajouter un écouteur d'événement sur le défilement de la page pour appeler la fonction
 window.addEventListener('scroll', afficherPresentation2);
