21c097d5ca0ff2eff77b2ad2e4f0d70663c1eab8
Maxvaucelle <125647489+Maxvaucelle@users.noreply.github.com>
2023-07-27 21:34:32 +0200

    media terminées

--- JS/menu-deroulant.js
+++ JS/menu-deroulant.js
@@ -1,5 +1,12 @@
-let btn_open = document.getElementById('openAside');
-let isActive = false;
-function openAside(){
 
-}
+document.querySelector("#openAside").addEventListener('click', () => {
+    const nav = document.querySelector('nav');
+    const toggle = nav.classList.toggle('active');
+    const btn_open = document.getElementById('openAside');
+    console.log(toggle)
+    if (toggle) {
+        btn_open.src = '/Ressources/icones/icons8-flèche-bas-64.png';
+    } else {
+        btn_open.src ='/Ressources/icones/icons8-flèche-haut-64.png';
+    }
+} )
